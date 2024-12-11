b082b5d4ce8a3f830a14ac486b99c6164a359f4b
Jakub Delicat <jakub.delicat@husarion.com>
2024-12-11 12:11:18 +0000

    Removed bt manager .cpp
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

--- husarion_ugv_manager/include/husarion_ugv_manager/behavior_tree_manager.hpp
+++ husarion_ugv_manager/include/husarion_ugv_manager/behavior_tree_manager.hpp
@@ -58,7 +58,12 @@ public:
    *
    * @param factory The factory object used to create the tree.
    */
-  void Initialize(BT::BehaviorTreeFactory & factory);
+  void Initialize(BT::BehaviorTreeFactory & factory)
+  {
+    config_ = CreateBTConfig(initial_blackboard_);
+    tree_ = factory.createTree(tree_name_, config_.blackboard);
+    groot_publisher_ = std::make_unique<BT::Groot2Publisher>(tree_, groot_port_);
+  }
 
   void TickOnce() { tree_status_ = tree_.tickOnce(); }
   void TickExactlyOnce() { tree_status_ = tree_.tickExactlyOnce(); }
@@ -80,7 +85,36 @@ protected:
    * entry type.
    * @return A BehaviorTree configuration object.
    */
-  BT::NodeConfig CreateBTConfig(const std::map<std::string, std::any> & bb_values) const;
+  BT::NodeConfig CreateBTConfig(const std::map<std::string, std::any> & bb_values) const
+  {
+    BT::NodeConfig config;
+    config.blackboard = BT::Blackboard::create();
+
+    for (auto & [name, value] : bb_values) {
+      const std::type_info & type = value.type();
+      if (type == typeid(bool)) {
+        config.blackboard->set<bool>(name, std::any_cast<bool>(value));
+      } else if (type == typeid(int)) {
+        config.blackboard->set<int>(name, std::any_cast<int>(value));
+      } else if (type == typeid(unsigned)) {
+        config.blackboard->set<unsigned>(name, std::any_cast<unsigned>(value));
+      } else if (type == typeid(float)) {
+        config.blackboard->set<float>(name, std::any_cast<float>(value));
+      } else if (type == typeid(double)) {
+        config.blackboard->set<double>(name, std::any_cast<double>(value));
+      } else if (type == typeid(const char *)) {
+        config.blackboard->set<std::string>(name, std::any_cast<const char *>(value));
+      } else if (type == typeid(std::string)) {
+        config.blackboard->set<std::string>(name, std::any_cast<std::string>(value));
+      } else {
+        throw std::invalid_argument(
+          "Invalid type for blackboard entry. Valid types are: bool, int, unsigned, float, double, "
+          "const char*, std::string");
+      }
+    }
+
+    return config;
+  }
 
 private:
   const std::string tree_name_;

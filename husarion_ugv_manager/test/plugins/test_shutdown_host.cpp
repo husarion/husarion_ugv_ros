7a29e290bfc3fbb3b83a8f353508205601635183
rafal.gorecki <rafal.gorecki@husarion.com>
2025-04-01 16:10:10 +0200

    Fix test

--- husarion_ugv_manager/test/plugins/test_shutdown_host.cpp
+++ husarion_ugv_manager/test/plugins/test_shutdown_host.cpp
@@ -121,7 +121,6 @@ TEST_F(TestShutdownHost, HTTPServerNotAvailable)
   ASSERT_EQ(this->shutdown_host_->GetState(), husarion_ugv_manager::ShutdownHostState::FAILURE);
   EXPECT_TRUE(this->ContainsExpression(this->shutdown_host_->GetError(), "Command return code:"));
   EXPECT_TRUE(this->ContainsExpression(this->shutdown_host_->GetOutput(), "000"));
-  EXPECT_TRUE(this->ContainsExpression(this->shutdown_host_->GetOutput(), "Connection refused"));
 }
 
 TEST_F(TestShutdownHost, GoodShutdownExecute)

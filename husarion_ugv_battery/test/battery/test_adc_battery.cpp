c49dfa72b4ca2eaba31c372f42f12519ef104eca
Infra Man <167307587+husarion-inframan@users.noreply.github.com>
2025-06-11 12:53:42 +0200

    fix charging status charging detection (#542) (#578)
    (cherry picked from commit 7ac2956d1ca6674680d94a7918fe94a201762cc0)
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>

--- husarion_ugv_battery/test/battery/test_adc_battery.cpp
+++ husarion_ugv_battery/test/battery/test_adc_battery.cpp
@@ -276,7 +276,13 @@ TEST_F(TestADCBattery, ChargingStatusMsgCharging)
 
 TEST_F(TestADCBattery, ChargingStatusMsgNotCharging)
 {
-  UpdateBattery(1.5, 0.01, 0.98, 0.04, false);
+  // Charger connected but not charging
+  UpdateBattery(1.5, 0.01, 0.98, 0.04, true);
+
+  EXPECT_EQ(charging_status_.charging, false);
+
+  // Charger disconnected
+  UpdateBattery(1.5, 0.01, 0.98, 0.5, false);
 
   EXPECT_FALSE(charging_status_.charging);
 }

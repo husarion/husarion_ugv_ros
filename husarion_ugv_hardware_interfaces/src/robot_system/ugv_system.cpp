e11a614829d25984bd8660c5bb174f9e4f053889
Dawid Kmak <73443304+KmakD@users.noreply.github.com>
2025-07-07 11:53:02 +0200

    Move hw activate (#585)

--- husarion_ugv_hardware_interfaces/src/robot_system/ugv_system.cpp
+++ husarion_ugv_hardware_interfaces/src/robot_system/ugv_system.cpp
@@ -80,22 +80,6 @@ CallbackReturn UGVSystem::on_configure(const rclcpp_lifecycle::State &)
     return CallbackReturn::ERROR;
   }
 
-  return CallbackReturn::SUCCESS;
-}
-
-CallbackReturn UGVSystem::on_cleanup(const rclcpp_lifecycle::State &)
-{
-  robot_driver_->Deinitialize();
-  robot_driver_.reset();
-
-  gpio_controller_.reset();
-  e_stop_.reset();
-
-  return CallbackReturn::SUCCESS;
-}
-
-CallbackReturn UGVSystem::on_activate(const rclcpp_lifecycle::State &)
-{
   std::fill(hw_commands_velocities_.begin(), hw_commands_velocities_.end(), 0.0);
   std::fill(hw_states_positions_.begin(), hw_states_positions_.end(), 0.0);
   std::fill(hw_states_velocities_.begin(), hw_states_velocities_.end(), 0.0);
@@ -139,7 +123,7 @@ CallbackReturn UGVSystem::on_activate(const rclcpp_lifecycle::State &)
   auto e_stop_reset_qos = rclcpp::ServicesQoS();
   e_stop_reset_qos.keep_last(1);
   system_ros_interface_->AddService<TriggerSrv, std::function<void()>>(
-    "hardware/e_stop_reset", std::bind(&EStopInterface::ResetEStop, e_stop_), 2,
+    "hardware/e_stop_reset", std::bind(&UGVSystem::ResetEStop, this), 2,
     rclcpp::CallbackGroupType::MutuallyExclusive, e_stop_reset_qos);
 
   system_ros_interface_->AddDiagnosticTask(
@@ -159,6 +143,24 @@ CallbackReturn UGVSystem::on_activate(const rclcpp_lifecycle::State &)
   return CallbackReturn::SUCCESS;
 }
 
+CallbackReturn UGVSystem::on_cleanup(const rclcpp_lifecycle::State &)
+{
+  robot_driver_->Deinitialize();
+  robot_driver_.reset();
+
+  gpio_controller_.reset();
+
+  system_ros_interface_.reset();
+  e_stop_.reset();
+
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn UGVSystem::on_activate(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
 CallbackReturn UGVSystem::on_deactivate(const rclcpp_lifecycle::State &)
 {
   try {
@@ -167,8 +169,6 @@ CallbackReturn UGVSystem::on_deactivate(const rclcpp_lifecycle::State &)
     RCLCPP_ERROR_STREAM(logger_, "Deactivation failed: " << e.what());
     return CallbackReturn::ERROR;
   }
-
-  system_ros_interface_.reset();
 
   return CallbackReturn::SUCCESS;
 }
@@ -263,9 +263,10 @@ return_type UGVSystem::read(const rclcpp::Time & time, const rclcpp::Duration & 
 
 return_type UGVSystem::write(const rclcpp::Time & /* time */, const rclcpp::Duration & /* period */)
 {
+  const auto lifecycle_state = this->get_lifecycle_state().id();
   const bool e_stop = e_stop_->ReadEStopState();
 
-  if (!e_stop) {
+  if (lifecycle_state == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE && !e_stop) {
     HandleRobotDriverWriteOperation([this] {
       const auto speed_cmds = GetSpeedCommands();
       robot_driver_->SendSpeedCommands(speed_cmds);
@@ -454,6 +455,18 @@ void UGVSystem::ConfigureEStop()
     std::bind(&UGVSystem::AreVelocityCommandsNearZero, this));
 
   RCLCPP_INFO(logger_, "Successfully configured E-Stop");
+}
+
+void UGVSystem::ResetEStop()
+{
+  const auto lifecycle_state = this->get_lifecycle_state().id();
+
+  if (lifecycle_state != lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE) {
+    throw std::runtime_error(
+      "Can't reset E-Stop when the hardware interface is not in ACTIVE state.");
+  }
+
+  e_stop_->ResetEStop();
 }
 
 void UGVSystem::UpdateMotorsState()

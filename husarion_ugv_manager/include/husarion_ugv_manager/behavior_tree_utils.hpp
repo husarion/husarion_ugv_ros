552a79662c374aada96b3b4ab6aa990a3687c05e
Infra Man <167307587+husarion-inframan@users.noreply.github.com>
2025-08-01 12:20:04 +0200

    Fix headers guards (#598) (#605)
    * Fix headers guards
    
    
    
    * Try and catch added
    
    
    
    ---------
    
    
    (cherry picked from commit 1c6a7a61279ee9812d691924967b36d902f61a64)
    
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>
    Co-authored-by: Jakub Delicat <109142865+delihus@users.noreply.github.com>

--- husarion_ugv_manager/include/husarion_ugv_manager/behavior_tree_utils.hpp
+++ husarion_ugv_manager/include/husarion_ugv_manager/behavior_tree_utils.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
-#define HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
+#ifndef HUSARION_UGV_MANAGER_HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
+#define HUSARION_UGV_MANAGER_HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
 
 #include <any>
 #include <chrono>
@@ -175,4 +175,4 @@ inline geometry_msgs::msg::PoseStamped convertFromString<geometry_msgs::msg::Pos
 
 }  // namespace BT
 
-#endif  // HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
+#endif  // HUSARION_UGV_MANAGER_HUSARION_UGV_MANAGER_BEHAVIOR_TREE_UTILS_HPP_
